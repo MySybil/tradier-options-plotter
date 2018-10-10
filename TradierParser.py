7597c2b69abd316de753ced93624173ef0e51f7b
Teddy Rowan <teddy@teddyrowan.com>
2018-10-10 00:45:24 -0400

    Added is_number() to check / convert parsed strings into floats if appropriate. Cleaned it up a lot.

--- TradierParser.py
+++ TradierParser.py
@@ -1,24 +1,56 @@
 # need to create a class and then import it and save this to it.
 
 # parsing:
-#  symbol / description / type / last / change / change_percentage / volume / trade_date / open / high / low / close / prevclose
+#  symbol / description / last / change / change_percentage / volume / trade_date / open / high / low / close / prevclose / bid / bidsize / ask / asksize
 
 # not parsing (for now):
-#  exch / average_volume / last_volume / week_52_low / week_52_high / bid / bidsize / bid_date / bidexch / ask / asksize / ask_date / askexch / root_symbols
+#  exch / type / average_volume / last_volume / week_52_low / week_52_high / bid_date / bidexch / ask_date / askexch / root_symbols
 
-def parseQuote(data):
+# Takes API Response from Tradier /quotes? endpoint and formats the desired data.
+# Currently only works for a single quote (options work)
+def parse_quote(data):
     #print(type(data))
     #print(data)
     
-    targetList = ["symbol", "description", "type", "last", "change", "change_percentage", "volume", "trade_date", "open", "high", "low", "close", "prevclose"]
+    targetList = ["symbol", "description", "last", "change", "change_percentage", "volume", "trade_date", "open", "high", "low", "close", "prevclose", "bid", "bidsize", "ask", "asksize"]
+    
+    # need to set up looping so that it works for multiple quotes.
     
     for x in targetList:
-      find_value(data, x)   
+      value = parse_target(data, x)   
+      print(x + ": " + value)
+      #print(type(value))
     
     
-def find_value(source, target):
+# Takes in the source download from the Tradier API and searches + parses it for the target and returns the target as a string.
+# Target demo: "symbol" to parse "<symbol>AAPL</symbol>"
+def parse_target(source, target):
     start = source.find("<" + target + ">") + len(target) + 2
     end = source.find("</" + target + ">")
     value = source[start:end]
-    print(target + ": " + value)
     
+    return(value)
+    ## this works but is a bit annoying b/c of types when printing after the value is returned.
+    ## prefer to just leave it as a string for now
+    #if is_number(value):
+    #    return float(value)
+    #else:
+    #    return(value)
+    
+
+# Returns true if the input is a number, false if not. Used to differentiate between string and floats that get parsed b/c the floats that get parsed end up stored as strings    
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+ 
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+ 
+    return False
--- basic_testing.py
+++ basic_testing.py
@@ -2,12 +2,6 @@ import requests
 import TradierParser
 #runs with python3
 
-# for some reason i can't get any function not to throw errors everywhere. standard me not knowing python..
-#def my_function(fname):
-#  print(fname + " Refsnes"
-  #return
-
-
 # NEWS API works.
 #r = requests.get("https://newsapi.org/v2/everything?q=Apple&from=2018-09-25&sortBy=popularity&apiKey=4583ba3bea6c4524b08aa8eed9479b07")
 #print (r.status_code)
@@ -17,7 +11,8 @@ import TradierParser
 
 # Dictionaries are now done with colons not commas.
 my_headers = {'Authorization': 'Bearer 5f1ga0KR0Ys1YlQhWtRAQAPKW8Iy'}
-url = "https://sandbox.tradier.com/v1/markets/quotes?symbols=MU"
+#url = "https://sandbox.tradier.com/v1/markets/quotes?symbols=MU"
+url = "https://sandbox.tradier.com/v1/markets/quotes?symbols=SPY190118C00300000"
 r = requests.get(url, headers=my_headers)
 
 
@@ -27,10 +22,5 @@ r = requests.get(url, headers=my_headers)
 #print (r.content)
 
 # need to decode the data from bytes into a string.
-TradierParser.parseQuote(r.content.decode("utf-8")) # file name + function name
-
-    
-#my_function("blegsfs")
-#return
-    
+TradierParser.parse_quote(r.content.decode("utf-8")) # file name + function name
 
