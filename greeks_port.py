fecd8443cc3d518dea8a4444cd72c3788c83b0c5
Teddy Rowan <teddy@teddyrowan.com>
2018-10-15 18:02:04 -0400

    Cleaned up greeks_port comments and print statements

--- greeks_port.py
+++ greeks_port.py
@@ -1,5 +1,8 @@
 import math
-from scipy.stats import norm #norm.cdf(x) # cool
+from scipy.stats import norm
+
+# swap to brent root finding or newton's method. 
+# start with initial guess. 
 
 # Binary-Search algorithm to backsolve implied-volatility in the BS-Model. Bases IV on the midpoint of the bid/ask spread.
 # Usually takes about 15 loops, so I haven't bothered to optimize away from Binary-Search (not sure what you folks will be doing)
@@ -41,28 +44,35 @@ def calculateIV(spotPrice, strike, years, div, midpoint, RFR, isCall):
         #print("value: " + str(val))
         
         # With low liquidity options, the midpoint can sometimes be less than intrinsic value, in this case I flag the option so I know that the IV is sketchy, but I would still prefer to have a value so I run calculateIV() again but with the ask price substituted for the midpoint.
+        # Ha, nevermind, I don't anymore. 
         if (IVGuess < 0.001):
             print("IV < 0.1 %.") # Probably deep ITM / OTM, maybe issues with spread
             return 0
         
         iterations += 1
-        if (iterations > 100): # Legacy that I don't think ever hits anymore.
+        if (iterations > 100): # Legacy that I don't think ever hits anymore. Actually it hits a lot but mainly when I fuck up parameters (*cough* isCall *cough*)
             print("Shit is broken. IV calc max iterations hit. Return 0")
             return 0
     
     return IVGuess
 
 
-## test1
-## expected output: 20%. real: 19.897%
-aa = calculateIV(14, 13, 0.5, 0.03, 1.34, 0.03, 1) #call example
-print("IV: " + str(aa*100) + " %. Expected output: 20.0%")
-## expected output: 20%. real: 20.081%
-bb = calculateIV(14, 13, 0.5, 0.03, 0.36, 0.03, 0) #put version
-print("IV: " + str(bb*100) + " %. Expected output: 20.0%")
+import time
+start_time = time.time()
+
+# def calculateIV(spotPrice, strike, years, div, midpoint, RFR, isCall):
+## test1 -- Idk just some generic values
+aa = calculateIV(14, 13, 0.5, 0.03, 1.34, 0.03, 1) #call example ## expected output: 20%. real: 19.897%
+bb = calculateIV(14, 13, 0.5, 0.03, 0.36, 0.03, 0) #put version ## expected output: 20%. real: 20.081%
 
 ## test2 -- CGC Jan 18, 2019 35C. $21.55 midpoint w/ IV 81.15 %. output: 81.21%
 aa2 = calculateIV(55.18, 35, 94/365.24, 0.00, 21.54, 0.02, 1) #call example
-print("IV: " + str(aa2*100) + " %. Expected output: 81.15%")
 bb2 = calculateIV(55.18, 35, 94/365.24, 0.00, 1.18, 0.02, 0) #put version. output: 81.21%
-print("IV: " + str(bb2*100) + " %. Expected output: 81.15%")
+
+#taking about 0.027s to run the 4 examples pre-optimiation
+print("--- %s seconds ---" % (time.time() - start_time))
+print("Test1 Call IV: %s %%. Expected output: 20.0%%" % str(aa*100))
+print("Test1 Put IV: %s %%. Expected output: 20.0%%" % str(bb*100))
+print("Test2 Call IV: %s %%. Expected output: 81.15%%" % str(aa2*100))
+print("Test2 Put IV: %s %%. Expected output: 81.15%%" % str(bb2*100))
+
