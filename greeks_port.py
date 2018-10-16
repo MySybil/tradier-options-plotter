2f182a668000cd39a3f23b7ca4b3cb8230aa991e
Teddy Rowan <teddy@teddyrowan.com>
2018-10-15 20:03:40 -0400

    Added initial approximation to speed up iterations. Not much of an improvement

--- greeks_port.py
+++ greeks_port.py
@@ -13,12 +13,21 @@ def calculateIV(spotPrice, strike, years, div, midpoint, RFR, isCall):
     # Min IV and Max Possible IV.
     knownMin = 0        # IV has to be above 0
     knownMax = 10.0     # cut in half right away -- valid for IV up to 500%.
-    IVGuess = knownMax
+    IVGuess = knownMax  # overwritten if using initial approx
     
-    # This is off by a pretty large bit if not very close to ATM. Need to adjust the rest as well if I choose to use it.
-    #double IVApprox = sqrt(2*M_PI/years_to_expiry)*midpoint/strike;
-    #NSLog(@"initial iv approx: %.2f %%", IVApprox*100); //
     
+    # Use an initial approximation. Doesn't really cut the time down by much... change to brent maybe?
+    # iteration numbers are lower, but only like 20%. 
+    IVGuess = math.sqrt(2*3.1415/years)*(midpoint/strike)
+    d1 = (math.log(spotPrice/strike) + years*(RFR-div+(IVGuess*IVGuess)/2))/(IVGuess*math.sqrt(years))
+    d2 = d1 - IVGuess*math.sqrt(years)
+    if isCall:
+        val = spotPrice*math.exp(-div*years)*norm.cdf(d1) - strike*math.exp(-RFR*years)*norm.cdf(d2)
+    else:
+        val = strike*math.exp(-RFR*years)*norm.cdf(-d2) - spotPrice*math.exp(-div*years)*norm.cdf(-d1)
+    difference = val - midpoint
+    
+
     iterations = 0
     
     while (abs(difference) > tolerance):
@@ -46,7 +55,7 @@ def calculateIV(spotPrice, strike, years, div, midpoint, RFR, isCall):
         # With low liquidity options, the midpoint can sometimes be less than intrinsic value, in this case I flag the option so I know that the IV is sketchy, but I would still prefer to have a value so I run calculateIV() again but with the ask price substituted for the midpoint.
         # Ha, nevermind, I don't anymore. 
         if (IVGuess < 0.001):
-            print("IV < 0.1 %.") # Probably deep ITM / OTM, maybe issues with spread
+            #print("IV < 0.1 %.") # Probably deep ITM / OTM, maybe issues with spread
             return 0
         
         iterations += 1
@@ -54,6 +63,7 @@ def calculateIV(spotPrice, strike, years, div, midpoint, RFR, isCall):
             print("Shit is broken. IV calc max iterations hit. Return 0")
             return 0
     
+    #print("Done. IV: " + str(IVGuess) + ". Iterations: " + str(iterations))
     return IVGuess
 
 
@@ -75,4 +85,3 @@ print("Test1 Call IV: %s %%. Expected output: 20.0%%" % str(aa*100))
 print("Test1 Put IV: %s %%. Expected output: 20.0%%" % str(bb*100))
 print("Test2 Call IV: %s %%. Expected output: 81.15%%" % str(aa2*100))
 print("Test2 Put IV: %s %%. Expected output: 81.15%%" % str(bb2*100))
-
